36185ac655f47b6ad28adc74304abbbd7370cec7
tomoki0215 <52097994+tomoki0215@users.noreply.github.com>
2021-12-19 16:03:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,26 +3,23 @@ from os import getenv
 import traceback
 import discord
 
+token = getenv('DISCORD_BOT_TOKEN')
+
 bot = commands.Bot(command_prefix='/')
-
-
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
-
-token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
 
-client = discord.Client()
 
+client = discord.Client()
 # チャンネル入退室時の通知処理
 @client.event
 async def on_voice_state_update(member, before, after):
